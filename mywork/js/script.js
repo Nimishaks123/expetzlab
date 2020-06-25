f60d6fa11507a65dc8a7e3ce8b66f2d6f86160c3
nimisha <lakshmimanoj203@gmail.com>
2020-06-25 12:43:25 +0530

    added extra javascript

---
+++ mywork/js/script.js
@@ -0,0 +1,13 @@
+function myfunc()
+{
+    alert("welcome")
+}
+function getname()
+{
+    var name=document.getElementById('text1').value
+    alert("hey.."+name)
+}
+function changeStyle()
+{
+    document.getElementById("p1").style.color="blue"
+}
